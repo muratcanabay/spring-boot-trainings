72d5e5095d5c4a20446e73380155f4cadcc9bfb4
muratcanabay <muratcanabay@hotmail.com>
2022-02-04 02:52:40 +0300

    initial commit

---
+++ src/test/java/com/mabay/springbootapp/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mabay.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
